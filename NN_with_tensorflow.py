c9f672197c219a37e6834dec7b6fd7fee275519f
Pierre Colonna D'Istria <pierre.col.di@gmail.com>
2020-05-31 23:04:24 +0200

    finished init and initialise

--- NN_with_tensorflow.py
+++ NN_with_tensorflow.py
@@ -13,9 +13,27 @@ class NeuralNetwork:
     """
 
     def __init__(self, layers):
+        
         self.layers = layers
         self.L = len(layers)    # Number of layers, "depth" of the network
 
         # We define the number of features, or input, and number of classes, or output.
         self.nb_features = layers[0]
         self.nb_classes = layers[-1]
+
+        # We will store here the weights and biaises
+        self.W = {}
+        self.b = {}
+
+        # And here the derivate of the weights and biaises
+        self.dW = {}
+        self.db = {}
+
+        # And finally, run our initialisation function
+        self.initialise()
+
+    def initialise(self):
+        """ We will iterate for each layer and initialize with a random value from a normal distribution"""
+        for i in range(1, self.L):
+            self.W[i] = tf.Variable(tf.random.normal(shape=(self.layers[i], self.layers[i-1])))
+            self.b[i] = tf.Variable(tf.random.normal(shape=(self.layers[i], 1)))
